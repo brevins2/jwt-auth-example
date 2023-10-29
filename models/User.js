b926f57f1b1da72d5ea0c7b52d518db6ec5aadfe
brevins2 <kiggundubrevin@gmail.com>
2023-10-29 22:22:43 +0300

    database edits

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
-const db = require('../db');
+// const db = require('../db');
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "jwt_auth_example",
+});
 
 const User = {};
 
 User.create = (username, email, password, callback) => {
-  db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+  db.query(
+    "INSERT INTO users VALUES (?, ?, ?)",
     [username, email, password],
     (err, results) => {
       if (err) return callback(err);
@@ -13,11 +23,15 @@ User.create = (username, email, password, callback) => {
 };
 
 User.findByUsername = (username, callback) => {
-  db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
-    if (err) return callback(err);
-    if (results.length === 0) return callback(null, null);
-    return callback(null, results[0]);
-  });
+  db.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    (err, results) => {
+      if (err) return callback(err);
+      if (results.length === 0) return callback(null, null);
+      return callback(null, results[0]);
+    }
+  );
 };
 
 module.exports = User;
